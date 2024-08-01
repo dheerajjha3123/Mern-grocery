3e7deabf6038a479c9e3fec3c9dde8251fa4a620
dheerajjha3123 <113532704+dheerajjha3123@users.noreply.github.com>
2024-08-01 18:55:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: "10mb" }));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
