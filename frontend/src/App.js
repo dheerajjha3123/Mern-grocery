21bafb679c984e4c9ee18ee7c1f7399ea67de75a
dheerajjha3123 <113532704+dheerajjha3123@users.noreply.github.com>
2024-08-01 19:10:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,39 @@
-//import logo from './logo.svg';
 import './App.css';
 import Header from './component/header';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import { setDataProduct } from './redux/productSlice';
-import {useDispatch,useSelector} from "react-redux"
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch()
-  const productData = useSelector((state)=>state.product)
+  const dispatch = useDispatch();
+  const productData = useSelector((state) => state.product);
 
-
-  useEffect(()=>{
-    (async()=>{
-      const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`)
-      const resData = await res.json()
-      //console.log(resData)
-      dispatch(setDataProduct(resData))
-    })()
-
-  },[])
+  useEffect(() => {
+    (async () => {
+      const baseUrl = process.env.REACT_APP_SERVER_DOMAIN;
+      const productUrl = new URL('/product', baseUrl);
+      const res = await fetch(productUrl);
+      if (res.ok) {
+        const resData = await res.json();
+        dispatch(setDataProduct(resData));
+      } else {
+        console.error('Failed to fetch product data:', res.status, res.statusText);
+      }
+    })();
+  }, [dispatch]);
 
   return (
-
     <>
-    <Toaster />
-    <div >
-     <Header/>  
-     <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
-         <Outlet/>
-      </main>  
-    </div>
-
+      <Toaster />
+      <div>
+        <Header />
+        <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+          <Outlet />
+        </main>
+      </div>
     </>
-
   );
 }
 
